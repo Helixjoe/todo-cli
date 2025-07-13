495073467b340d7a69b4e240a56815c336d43c6e
Helixjoe <joeljohn5112@gmail.com>
2025-07-13 18:27:37 +0530

    storage: save & load methods

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 func main() {
 	todo := TodoList{}
+	storage := NewStorage[TodoList]("todo.json")
+	storage.Load(&todo)
 	todo.add("Buy Groceries")
 	todo.add("Go to the Gym")
 	todo.toggle(0)
 	todo.print()
+	storage.Save(todo)
 }
