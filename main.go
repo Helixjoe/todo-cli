ee525afc3fdbccfe5305c6fd674cfd4ace870408
Helixjoe <joeljohn5112@gmail.com>
2025-07-24 13:23:10 +0530

    todos & todolists check

--- main.go
+++ main.go
@@ -1,10 +1,55 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
 func main() {
-	todo := TodoList{}
-	storage := NewStorage[TodoList]("todo.json")
-	storage.Load(&todo)
-	commands := NewCmdFlags()
-	commands.Execute(&todo)
-	storage.Save(todo)
+	//todo-directory check
+	files, err := os.ReadDir(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+	todoFound := false
+	for _, file := range files {
+		if file.Name() == "todos"{
+			todoFound = true
+			fmt.Println("todos directory found")
+		}
+	}
+	if !todoFound {
+		fmt.Println("todos directory not found")
+		os.Mkdir("todos",0750)
+		fmt.Println("todos directory created")
+	}
+
+	//todolists check
+	files, err = os.ReadDir("todos")
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	if len(files) == 0 {
+		fmt.Println("0 todolists found")
+		var newTodoName string
+		fmt.Println("Enter name for new todolist:")
+		fmt.Scan(&newTodoName)
+		err := os.WriteFile("todos/"+newTodoName + ".json", []byte("{}"), 0650)
+		if err != nil {
+			fmt.Println("Failed to create file:", err)
+		}			
+		} else {
+		fmt.Println("Todolists found are:")
+		for _,file := range files {
+			fmt.Println(file.Name())
+		}
+	}
+	// todo := TodoList{}
+	// storage := NewStorage[TodoList]("todo.json")
+	// storage.Load(&todo)
+	// commands := NewCmdFlags()
+	// commands.Execute(&todo)
+	// storage.Save(todo)
 }
